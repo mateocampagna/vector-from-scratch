3fde28b4142ced6812709a8226d8455ac8809e49
mateocampagna <matecamp4@gmail.com>
2026-07-17 15:29:19 -0300

    refactor & clean code

--- src/vector.rs
+++ src/vector.rs
@@ -5,68 +5,53 @@ use std::alloc::{self, Layout};
 use std::ops::Deref;
 use std::ops::DerefMut;
 
+// -------------- Structs
 pub struct Vector<T> {
-  ptr: NonNull<T>, 
-  cap: usize,
+  buffer:RawVector<T>,
   len: usize,
 }
 
+pub struct RawVector<T>{
+  ptr:NonNull<T>, 
+  cap:usize,
+}
+
+pub struct IntoIter<T>{
+  _buffer:RawVector<T>, 
+  start:*const T, 
+  end:*const T, 
+}
+
+// -------------- impls
+
 impl<T> Vector<T>{
-  pub fn new() -> Vector<T>{
-    assert!(mem::size_of::<T>() != 0, "we're not ready to handle zero-sized types");
-    Vector { 
-      ptr: NonNull::dangling(), 
-      cap: 0, 
-      len: 0, 
+  fn ptr(&self) -> *mut T {
+    self.buffer.ptr.as_ptr()
+  }
+
+  fn cap(&self) -> usize {
+    self.buffer.cap
+  }
+
+  pub fn new() -> Self {
+    Vector{
+      buffer: RawVector::new(),
+      len: 0,
     }
   }
 
-  // el vector se quedo sin capacidad y pide mas memoria
-  fn grow(&mut self){
-    // calcular nueva capacidad (pido el doble)
-    let(new_cap, new_layout) = 
-      if self.cap == 0 {
-        (1, Layout::array::<T>(1))
-      }
-      else{
-        let new_cap = 2*self.cap;
-        (new_cap, Layout::array::<T>(new_cap))
-      };
-    
-    let new_layout = new_layout.expect("allocation too large!"); 
-    // elegir entre alloc (primera vez) o realloc (crecer)
-    let new_ptr = 
-      if self.cap == 0{
-        unsafe {alloc::alloc(new_layout)}
-      }
-      else{
-        let old_layout = Layout::array::<T>(self.cap).unwrap();
-        let old_ptr = self.ptr.as_ptr() as *mut u8;
-        // para poder reubicar le paso el ptr viejo, el layout viejo y el tam. nuevo, asi copia los datos
-        // y mueve la alocacion
-        unsafe { alloc::realloc(old_ptr, old_layout, new_layout.size()) }
-      };
-    
-    // si la alocacion fallo entonces el nuevo ptr es null, abortamos
-    self.ptr = match NonNull::new(new_ptr as *mut T) {
-      Some(p) => p, 
-      None => alloc::handle_alloc_error(new_layout),
-    };
-
-    self.cap = new_cap
-  }
 
   pub fn push(&mut self, value:T){
     // si estamos full -> expando
-    if self.len == self.cap{
-      self.grow();
+    if self.len == self.cap(){
+      self.buffer.grow();
     }
 
     unsafe{
       // escribe en memoria sin leer ni tirar el valor antiguo
       // .add() es la version usize de .offset() (isize)
       // va desde el self.ptr hasta la prox. pos. del slot libre (self.len) 
-      std::ptr::write(self.ptr.as_ptr().add(self.len), value);
+      std::ptr::write(self.ptr().add(self.len), value);
     }
   }
 
@@ -76,21 +61,21 @@ impl<T> Vector<T>{
     }
     else{
       self.len -= 1;
-      unsafe { Some(std::ptr::read(self.ptr.as_ptr().add(self.len))) }
+      unsafe { Some(std::ptr::read(self.ptr().add(self.len))) }
     }
   }
 
   pub fn insert(&mut self, idx:usize, value:T){
     assert!(idx <= self.len, "index out of bounds");
-    if self.len == self.cap{
-      self.grow();
+    if self.len == self.cap(){
+      self.buffer.grow();
     }
 
     unsafe{
       // ptr::copy(src, dest, len) -> copy from src to dest len elems
       std::ptr::copy(
-        self.ptr.as_ptr().add(idx), 
-        self.ptr.as_ptr().add(idx + 1), 
+        self.ptr().add(idx), 
+        self.ptr().add(idx + 1), 
         self.len - idx,
       )
     }
@@ -102,10 +87,10 @@ impl<T> Vector<T>{
     assert!(idx < self.len, "index out of bounds");
     unsafe {
       self.len -= 1;
-      let res = std::ptr::read(self.ptr.as_ptr().add(idx)); 
+      let res = std::ptr::read(self.ptr().add(idx)); 
       std::ptr::copy(
-        self.ptr.as_ptr().add(idx+1),
-        self.ptr.as_ptr().add(idx),
+        self.ptr().add(idx+1),
+        self.ptr().add(idx),
         self.len - idx,
       );
       return res;
@@ -115,10 +100,10 @@ impl<T> Vector<T>{
 
 impl<T> Drop for Vector<T>{
   fn drop(&mut self) {
-      if self.cap != 0 {
+      if self.cap() != 0 {
         while let Some(_) = self.pop() { }
-        let layout = Layout::array::<T>(self.cap).unwrap();
-        unsafe { alloc::dealloc(self.ptr.as_ptr() as *mut u8, layout);} 
+        let layout = Layout::array::<T>(self.cap()).unwrap();
+        unsafe { alloc::dealloc(self.ptr() as *mut u8, layout);} 
       }
   }
 }
@@ -128,50 +113,36 @@ impl<T> Deref for Vector<T>{
   type Target = [T];
   
   fn deref(&self) -> &[T]{
-    unsafe { std::slice::from_raw_parts(self.ptr.as_ptr(), self.len)}
+    unsafe { std::slice::from_raw_parts(self.ptr(), self.len)}
   }
 }
 
 // vista modificable &mut [10,20,30] -> v[0] = 100;
 impl<T> DerefMut for Vector<T>{
   fn deref_mut(&mut self) -> &mut [T]{
-    unsafe { std::slice::from_raw_parts_mut(self.ptr.as_ptr(), self.len)}
+    unsafe { std::slice::from_raw_parts_mut(self.ptr(), self.len)}
   }
-}
-
-unsafe impl<T: Send> Send for Vector<T> {}
-unsafe impl<T: Sync> Sync for Vector<T> {}
-
-
-pub struct IntoIter<T>{
-  buf:NonNull<T>, 
-  cap:usize,
-  start:*const T, 
-  end:*const T, 
 }
 
 impl<T> IntoIterator for Vector<T>{
-  type Item = T; 
-  type IntoIter = IntoIter<T>;
+    type Item = T;
+    type IntoIter = IntoIter<T>;
+    fn into_iter(self) -> IntoIter<T> {
+      let buf = unsafe { std::ptr::read(&self.buffer  ) };
+      let len = self.len;
+      
+      mem::forget(self);
 
-  fn into_iter(self) -> IntoIter<T>{
-    let vec = ManuallyDrop::new(self);
-    let ptr = vec.ptr;
-    let cap = vec.cap;
-    let len = vec.len;
-
-    IntoIter { 
-      buf: ptr, 
-      cap, 
-      start: ptr.as_ptr(), 
-      end:if cap == 0 {
-            ptr.as_ptr()
-          }
-          else{
-            unsafe { ptr.as_ptr().add(len) }
-          },
+      IntoIter{
+        start: buf.ptr.as_ptr(),
+        end: if buf.cap == 0 {
+            buf.ptr.as_ptr()
+        } else {
+            unsafe { buf.ptr.as_ptr().add(len) }
+        },
+        _buffer: buf,
+      }
     }
-  }
 }
 
 // iterating forward
@@ -216,13 +187,54 @@ impl<T> DoubleEndedIterator for IntoIter<T> {
 // Intoiter takes ownership of its alloc. -> Drop needed to free it
 impl<T> Drop for IntoIter<T> {
     fn drop(&mut self) {
-        if self.cap != 0 {
-            // drop any remaining elements
-            for _ in &mut *self {}
-            let layout = Layout::array::<T>(self.cap).unwrap();
-            unsafe {
-                alloc::dealloc(self.buf.as_ptr() as *mut u8, layout);
-            }
-        }
+      for _ in &mut *self {}
     }
 }
+
+
+impl<T> RawVector<T> {
+  fn new() -> Self {
+    assert!(mem::size_of::<T>() != 0, "TODO: implement ZST support");
+    RawVector { ptr: NonNull::dangling(), cap: 0 }
+  }
+  fn grow(&mut self) {
+    let new_cap = if self.cap == 0 { 1 } else { 2 * self.cap };
+    let new_layout = Layout::array::<T>(new_cap).unwrap();
+    assert!(new_layout.size() <= isize::MAX as usize, "Allocation too large");
+
+    let new_ptr = if self.cap == 0 {
+      unsafe { alloc::alloc(new_layout) }
+    }
+    else{
+      let old_layout = Layout::array::<T>(self.cap).unwrap();
+      let old_ptr = self.ptr.as_ptr() as *mut u8;
+      unsafe { alloc::realloc(old_ptr, old_layout, new_layout.size()) }
+    };
+
+    self.ptr = match NonNull::new(new_ptr as *mut T){
+      Some(p) => p,
+      None => alloc::handle_alloc_error(new_layout),
+    };
+    self.cap = new_cap;
+  }
+}
+
+impl<T> Drop for RawVector<T> {
+  fn drop(&mut self) {
+    if self.cap != 0 {
+      let layout = Layout::array::<T>(self.cap).unwrap();
+      unsafe {
+        alloc::dealloc(self.ptr.as_ptr() as *mut u8, layout);
+      }
+    }
+  }
+}
+
+
+// -------------- unsafe impls
+
+unsafe impl<T: Send> Send for Vector<T> {}
+unsafe impl<T: Sync> Sync for Vector<T> {}
+
+unsafe impl<T: Send> Send for RawVector<T> {}
+unsafe impl<T: Sync> Sync for RawVector<T> {}
